ddd956939b3d20c0d1f660340615ff476681e9bd
fritz-pal <henri@staudenrausch.de>
2023-03-22 16:54:23 +0100

    first window and tiles

---
+++ src/main/java/de/hhn/Tile.java
@@ -0,0 +1,21 @@
+package de.hhn;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Tile extends JPanel {
+
+    private int x;
+    private int y;
+    private int size;
+    private boolean isWhite;
+
+    public Tile(int x, int y, int size, boolean isWhite) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.isWhite = isWhite;
+        this.setBounds(x, y, size, size);
+        this.setBackground(isWhite ? new Color(0xedd6b0) : new Color(0xb88762));
+    }
+}
--- src/main/java/de/hhn/Window.java
+++ src/main/java/de/hhn/Window.java
@@ -1,16 +1,28 @@
 package de.hhn;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
     public Window() {
-        super("Schach");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1000, 1000);
-        setLocationRelativeTo(null);
+        super();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setSize(816, 839);
+        this.setLocationRelativeTo(null);
+        this.setLayout(null);
+        this.setTitle("Schach");
+        this.setResizable(false);
+        this.setIconImage(new ImageIcon("pawn_white+.svg").getImage());
+        this.getContentPane().setBackground(new Color(0xedd6b0));
 
-        setLayout(null);
-        setResizable(false);
 
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                Tile tile = new Tile(x*100, y*100, 100, (x + y) % 2 == 0);
+                this.add(tile);
+                System.out.println(this.getWidth() + " " + this.getHeight());
+            }
+        }
     }
 }
