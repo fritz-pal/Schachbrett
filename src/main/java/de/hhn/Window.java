4b4be8b40591dcd9d198b90aecf5f73f2d2482a5
fritz-pal <henri@staudenrausch.de>
2023-03-22 17:06:14 +0100

    mac compatibility

--- src/main/java/de/hhn/Window.java
+++ src/main/java/de/hhn/Window.java
@@ -7,7 +7,7 @@ public class Window extends JFrame {
     public Window() {
         super();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
+        this.setName("Schach");
         this.setSize(816, 839);
         this.setLocationRelativeTo(null);
         this.setLayout(null);
@@ -16,12 +16,10 @@ public class Window extends JFrame {
         this.setIconImage(new ImageIcon("pawn_white+.svg").getImage());
         this.getContentPane().setBackground(new Color(0xedd6b0));
 
-
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
                 Tile tile = new Tile(x * 100, y * 100, 100, (x + y) % 2 == 0);
                 this.add(tile);
-                System.out.printf("%d %d%n", this.getWidth(), this.getHeight());
             }
         }
     }
