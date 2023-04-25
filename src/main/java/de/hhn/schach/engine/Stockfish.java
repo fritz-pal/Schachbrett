f1acd638683162ae5addfa2f1fa56bc35b8f5266
fritz-pal <henri@staudenrausch.de>
2023-04-25 03:38:42 +0200

    fixed bug where program crashed after terminating engine

--- src/main/java/de/hhn/schach/engine/Stockfish.java
+++ src/main/java/de/hhn/schach/engine/Stockfish.java
@@ -4,10 +4,6 @@ import de.hhn.schach.Main;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
@@ -23,9 +19,6 @@ public class Stockfish extends Thread {
                     throw new RuntimeException(String.format("Program execution failed (code %d): %s", errorCode,
                             reader.lines().collect(Collectors.joining())));
                 }
-            } else {
-                System.out.println("Process terminated successfully.");
-                System.exit(0);
             }
         } catch (IOException e) {
             throw new RuntimeException("Could not invoke program.", e);
